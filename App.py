7467b6e1aae2ed91553304f746f812b120282fec
hashirSoomro <hashirsomroo@gmail.com>
2024-02-28 13:23:35 +0500

    Create Frontend for Ciphers
    Create Frontend for Ciphers using Streamlit.

---
+++ App.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from Lab1_CaeserCipher import encryption_C,decryption_C
+from Lab2_VigenereCipher import encryption,decryption
+
+def caeser_cipher():
+    st.title("Caeser Cipher")
+    option = st.radio("Select an option:", ("Encrypt", "Decrypt"))
+    if option == "Encrypt":
+        string_user = st.text_input("Enter the message to encrypt:")
+        if st.button("Encrypt"):
+            enc_str=encryption_C(string_user)
+            st.write("Here is your encrypted message:", enc_str)
+            
+    elif option == "Decrypt":
+        string_user = st.text_input("Enter the message to decrypt:")
+        if st.button("Decrypt"):
+            dec_str=decryption_C(string_user)
+            st.write("Here is your decrypted message:", dec_str)
+
+def vigenere_cipher():
+    st.title("Vigenere Cipher")
+    option = st.radio("Select an option:", ("Encrypt", "Decrypt"))
+    if option == "Encrypt":
+        plainText = st.text_input("Enter text you want to encrypt:")
+        keyword = st.text_input("Enter the keyword:")
+        if st.button("Encrypt"):
+            enc_text = encryption(plainText, keyword)
+            st.write("Encrypted Text:", enc_text)
+    elif option == "Decrypt":
+        cipherText = st.text_input("Enter text you want to decrypt:")
+        keyword = st.text_input("Enter the keyword:")
+        if st.button("Decrypt"):
+            dec_text = decryption(cipherText, keyword)
+            st.write("Decrypted Text:", dec_text)
+
+def polyAlphabetic_cipher():
+    st.title("PolyAlphabetic Cipher")
+    option = st.radio("Select an option:", ("Encrypt", "Decrypt"))
+    if option == "Encrypt":
+        plainText = st.text_input("Enter text you want to encrypt:")
+        keyword = st.text_input("Enter the keyword:")
+        if st.button("Encrypt"):
+            enc_text = encryption(plainText, keyword)
+            st.write("Encrypted Text:", enc_text)
+    elif option == "Decrypt":
+        cipherText = st.text_input("Enter text you want to decrypt:")
+        keyword = st.text_input("Enter the keyword:")
+        if st.button("Decrypt"):
+            dec_text = decryption(cipherText, keyword)
+            st.write("Decrypted Text:", dec_text)
+
+
+def main():
+    st.sidebar.title("Select Cipher")
+    selected_cipher = st.sidebar.radio("", ("Caeser Cipher", "Vigenere Cipher","PolyAlphabetic Cipher"))
+
+    if selected_cipher == "Caeser Cipher":
+        caeser_cipher()
+    elif selected_cipher == "Vigenere Cipher":
+        vigenere_cipher()
+    elif selected_cipher == "PolyAlphabetic Cipher":
+        polyAlphabetic_cipher()
+
+if __name__ == "__main__":
+    main()
--- Lab2_VigenereCipher.py
+++ Lab2_VigenereCipher.py
@@ -1,22 +1,34 @@
-text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-plainText=input("Enter text you want to encrypt: ")
-cipherText=input("Enter text you want to decrypt: ") #"GCYCZFMLYLEIM"
-keyword="AYUSH"
-key=""
-kcount=0
+#plainText=input("Enter text you want to encrypt: ")
+#cipherText=input("Enter text you want to decrypt: ") #"GCYCZFMLYLEIM"
+##keyword="AYUSH"
+##key=""
+##kcount=0
 
-#Generating key using keyword
-if (len(plainText)>len(keyword)) or (len(plainText)<len(keyword)):
-    for i in range(len(plainText)):
-        if kcount>=(len(keyword)-1):
-            kcount=-1
-        key=key+keyword[kcount]
-        kcount=kcount+1
-else:
-    key=keyword
+###Generating key using keyword
+##if (len(plainText)>len(keyword)) or (len(plainText)<len(keyword)):
+##    for i in range(len(plainText)):
+##        if kcount>=(len(keyword)-1):
+##            kcount=-1
+##        key=key+keyword[kcount]
+##        kcount=kcount+1
+##else:
+##    key=keyword
 
 #For encryption of text
-def encryption(plainText):
+def encryption(plainText,keyword):
+    text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key=""
+    kcount=0
+    #Generating key using keyword
+    if (len(plainText)>len(keyword)) or (len(plainText)<len(keyword)):
+        for i in range(len(plainText)):
+            if kcount>=(len(keyword)-1):
+                kcount=-1
+            key=key+keyword[kcount]
+            kcount=kcount+1
+    else:
+        key=keyword
+    
     encrypted_text=""
     for i in range(len(plainText)):#GEEKSFORGEEKS
         temp_text=""               #AYUSHAYUSHAYU
@@ -26,18 +38,28 @@ def encryption(plainText):
         for row_no in range(len(text)):
             if text[row_no]==plainText[i]:
                 temp_row_no=row_no
-                #print(temp_row_no)
+                
             if text[row_no]==key[i]:
                 temp_col_no=row_no
-                #print(temp_col_no)
             
         temp_text=text[temp_row_no:]+text[0:temp_row_no]
-        #print(temp_text)
         encrypted_text=encrypted_text+temp_text[temp_col_no]
     return ("Encrypted Text: "+encrypted_text)
 
 #for decryption of text
-def decryption(cipherText):
+def decryption(cipherText,keyword):
+    text="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    key=""
+    kcount=0
+    #Generating key using keyword
+    if (len(cipherText)>len(keyword)) or (len(cipherText)<len(keyword)):
+        for i in range(len(cipherText)):
+            if kcount>=(len(keyword)-1):
+                kcount=-1
+            key=key+keyword[kcount]
+            kcount=kcount+1
+    else:
+        key=keyword
     decrypted_text=""
     for i in range(len(cipherText)):#GCYCZFMLYLEIM
         temp_text=""                #AYUSHAYUSHAYU
@@ -54,6 +76,4 @@ def decryption(cipherText):
                 break
     return ("Decrypted Text: "+decrypted_text)
 
-print(encryption(plainText))
-print(decryption(cipherText))
 
